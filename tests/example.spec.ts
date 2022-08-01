1b5bcde8de1b62f26fcdc098085b9035532aa86a
Henry Xie <hman821223@gmail.com>
2022-08-01 22:23:00 +0800

    test click element

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,14 @@
-import { test, expect} from '@playwright/test'
+import { test, expect } from "@playwright/test";
 
-test('some basic test', async ({page}) => {
-    await page.goto('https://www.example.com')
-    const pageTitle = await page.locator('h1')
-    await expect(pageTitle).toContainText('Example Domain')
-})
+test("some basic test", async ({ page }) => {
+  await page.goto("https://www.example.com");
+  const pageTitle = await page.locator("h1");
+  await expect(pageTitle).toContainText("Example Domain");
+});
+
+test("click on element", async ({ page }) => {
+    await page.goto("https://www.pazzo.com.tw/login")
+    await page.click("#btnLogin")
+    const errorMessage = await page.locator(".popup")
+    await expect(errorMessage).toContainText("請輸入會員帳號!")
+});
